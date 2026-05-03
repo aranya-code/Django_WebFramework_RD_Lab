4f5e815b968585ac02c36a308909e25d459bc25e
Aranya Majumdar <74165561+aranya-code@users.noreply.github.com>
2026-05-03 21:53:55 +0530

    add docker and nginx

--- clinicalData/clinicalData/settings.py
+++ clinicalData/clinicalData/settings.py
@@ -83,6 +83,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'host.docker.internal', 
+        'PORT': '3306',
     }
 }
 
